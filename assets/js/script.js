ccea4a0828d8644e6624f9c87930ac119e0ec11f
fr3dy5 <fr3dy5al@gmail.com>
2024-12-11 21:40:08 -0600

    made a randomizer test function, and fixed typo and finished the guess checker function, changed a html tag id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let fetchedword = "";
 let scrambledword = "";
 let difficulty = "";
+let testword = "racecar";
 
 function ScrambledWord(fetchedword){
 
@@ -20,13 +21,28 @@ function fetchword(){
         }
 }
 
+
+
 function checkSubmit(){
-    let userGuess = document.getElementById("userGuess").ariaValueMax.toLowerCase();
+    let userGuess = document.getElementById("userGuess").value.toLowerCase();
     if (userGuess === fetchedword) {
         alert("You Win");
     }
     else{
         alert("Nope");
     }
+    document.getElementById("userGuess").value="";
 }
 
+
+
+function scrambler(testword){
+    const wordarray = testword.split('');
+    for(let i = wordarray.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i +  1));
+        [wordarray[i], wordarray[j]] = [wordarray[j], wordarray[i]];
+    }
+    return wordarray.join('');
+}
+scrambledword = scrambler(testword)
+console.log(scrambledword);
