ef26d1cb5a7e03636fadb67e266223d32ea94322
fr3dy5 <fr3dy5al@gmail.com>
2024-12-13 04:01:39 -0600

    Update api.js was testing the easy button but cant get it to work
    from vscode in browser

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,3 @@
 export const easyAPIcall = async () => {return fetch('https://random-word-api.herokuapp.com/word?length=4')};
-export const normalAPIcall = async () => await fetch('https://random-word-api.herokuapp.com/word?length=6');
-export const hardAPIcall = async () => await fetch('https://random-word-api.herokuapp.com/word?length=8');
+export const normalAPIcall = async () => {return fetch('https://random-word-api.herokuapp.com/word?length=6')};
+export const hardAPIcall = async () => {return fetch('https://random-word-api.herokuapp.com/word?length=8')};
