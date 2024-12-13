1ba99ea0b5863d5d8f2f30c60fe923fb78234593
fr3dy5 <fr3dy5al@gmail.com>
2024-12-13 03:58:28 -0600

    Update script.js
    copy to browser editor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,35 @@
+import { easyAPIcall, normalAPIcall, hardAPIcall } from 'assets/js/api.js';
+
 let fetchedword = "";
 let scrambledword = "";
-let difficulty = "";
-let testword = "racecar";
+let difficulty = "easy";
 
-function ScrambledWord(fetchedword){
-
-}
-
-function fetchword(){
+async function fetchword(){
+    let result;
+    try{
     switch(difficulty){
         case 'easy':
-
+            result = await easyAPIcall();
             break;
         case 'normal':
-
+            result = await normalAPIcall();
             break;
         case 'hard':
-
+            result = await hardAPIcall();
             break;
-        }
+    }
+    fetchedword = await result.text();
+    scrambledword = scrambler(fetchedword);
+    displayScramble();
+    }
+    catch (error){
+        console.error("fetch is not gonna happen stop trying to make happen", error);
+    }
 }
 
-
+function displayScramble(){
+    document.getElementById("scrambledWord").innerText=scrambledword;
+}
 
 function checkSubmit(){
     let userGuess = document.getElementById("userGuess").value.toLowerCase();
@@ -34,15 +42,38 @@ function checkSubmit(){
     document.getElementById("userGuess").value="";
 }
 
-
-
-function scrambler(testword){
-    const wordarray = testword.split('');
+function scrambler(fetchedword){
+    const wordarray = fetchedword.split('');
     for(let i = wordarray.length - 1; i > 0; i--){
         const j = Math.floor(Math.random() * (i +  1));
         [wordarray[i], wordarray[j]] = [wordarray[j], wordarray[i]];
     }
-    return wordarray.join('');
+    const scrambledword = wordarray.join('');
+    return scrambledword;
 }
-scrambledword = scrambler(testword)
-console.log(scrambledword);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const easyBtn = document.getElementById('easy');
+    const normalBtn = document.getElementById('normal');
+    const hardBtn = document.getElementById('hard');
+    const submitBtn = document.getElementById('submit');
+    const resetBtn = document.getElementById('reset');
+    const scrambledWord = document.getElementById('scrambledWord');
+    const userGuess = document.getElementById('userGuess');
+
+    easyBtn.addEventListener("click", () => {
+        difficulty = "easy";
+        fetchAndScrambleWord(difficulty);
+    });
+    
+    normalBtn.addEventListener("click", () => {
+        difficulty = "normal";
+        fetchAndScrambleWord(difficulty);
+    });
+    
+    hardBtn.addEventListener("click", () => {
+        difficulty = "hard";
+        fetchAndScrambleWord(difficulty);
+    });
+
+})
